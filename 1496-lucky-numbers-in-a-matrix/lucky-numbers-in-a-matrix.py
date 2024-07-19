9dd302a09e3fb6dac30e41af190c55555824b70f
Suyash Gaikwad <97752627+Suyash-Gaikwad9@users.noreply.github.com>
2024-07-19 10:23:20 +0530

    Time: 122 ms (20.55%) | Memory: 16.9 MB (43.01%) - LeetSync

--- 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.py
@@ -6,15 +6,11 @@ class Solution:
         row_min = set()
         for r in range(ROWS):
             row_min.add(min(matrix[r]))
-        col_max = set()
         for c in range(COLS):
                 cur_max = matrix[0][c]
                 for r in range(ROWS):
                     cur_max = max(cur_max, matrix[r][c])
-                col_max.add(cur_max)
-
-        for n in col_max:
-            if n in row_min:
-                res.append(n)
+                if cur_max in row_min:
+                    res.append(cur_max)
         
         return res
